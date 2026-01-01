cabb638cef61f61bcbe75f6eb97866e72f16e0e9
akeem-ui <86857393+akeem-ui@users.noreply.github.com>
2026-01-01 22:23:44 +0100

    text center fixed

--- src/components/ui/herosection.jsx
+++ src/components/ui/herosection.jsx
@@ -50,9 +50,9 @@ export default function HeroSection() {
           data-driven products, automation tools, and educational platforms in
           Web3, digital finance, and emerging technologies.
         </p>
-      <div className="flex gap-4 flex-col md:flex-row flex-nowrap text-sm">
-          <a href="#product" className={`border block drop-shadow-lg hover:drop-shadow-lg hover:drop-shadow-white/20 drop-shadow-white/10 text-nowrap cursor-pointer border-solid md:text-[16px] bg-white/70 py-3 px-6 rounded-xl font-semibold text-black`}>Discover Our Product</a>
-          <a href="https://t.me/+PDthiNCZhTlmYTJk" target="_blank"  className={`cursor-pointer z-3 block hover:bg-white/10 font-light text-white text-nowrap md:text-[16px] py-3 px-6 rounded-xl bg-black/40 border border-white/20 `}>Join the Academy</a>
+      <div className="flex gap-4 flex-col items-center text-center md:flex-row flex-nowrap text-sm">
+          <a href="#product" className={`border block drop-shadow-lg text-center hover:drop-shadow-lg hover:drop-shadow-white/20 drop-shadow-white/10 text-nowrap cursor-pointer border-solid md:text-[16px] bg-white/70 py-3 px-6 rounded-xl font-semibold text-black`}>Discover Our Product</a>
+          <a href="https://t.me/+PDthiNCZhTlmYTJk" target="_blank"  className={`cursor-pointer text-center z-3 block hover:bg-white/10 font-light text-white text-nowrap md:text-[16px] py-3 px-6 rounded-xl bg-black/40 border border-white/20 `}>Join the Academy</a>
       </div>
       </div>
     </div>
